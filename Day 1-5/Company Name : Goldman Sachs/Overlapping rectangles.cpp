32fe890e514a24272c68ae8a23ad98bc294ba95d
Mridul Kapoor <66908728+mridul8920@users.noreply.github.com>
2022-01-01 17:42:48 +0530

    Create Overlapping rectangles.cpp

---
+++ Day 1-5/Company Name : Goldman Sachs/Overlapping rectangles.cpp
@@ -0,0 +1,18 @@
+Problem Link: https://practice.geeksforgeeks.org/problems/overlapping-rectangles1924/1/#
+
+Expected Time Complexity:O(1)
+Expected Auxillary Space:O(1)
+
+class Solution {
+  public:
+    int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+        // code here
+        //If one rectangle is on left side of other
+        if(L1[0]>R2[0] || L2[0]>R1[0])
+            return false;
+        //If one rectangle is above the other
+        if(L1[1]<R2[1] || L2[1]<R1[1])
+            return false;
+        return true;
+    }
+};
